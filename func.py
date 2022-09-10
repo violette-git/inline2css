751af3300f8f52d88d5ba7474702d0c6f5194f30
violette-git <ideasforyang@gmail.com>
2022-09-10 02:54:36 -0500

    initial

---
+++ func.py
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+def parse():
+    # do stuff
+    # return a list of all tags in html
+    pass
+
+
+def id_tags(): #?? order ??
+    #do stuff
+    # return list of all tags after given a new id
+    pass
+
+
+def count_tags(): #?? order ??
+    #do stuff
+    # return list of all tags and their count
+    pass
+
+
+def convert():
+    # do stuff
+    # return css file with all listed tags
+    pass
